7021e6768241cd6d80ac1a232512b3c0e2d2498b
saniya maner <saniyamaner13@gmail.com>
2026-06-20 21:58:50 +0530

    Create 183.Customers Who Never Order.sql

---
+++ 183.Customers Who Never Order.sql
@@ -0,0 +1,9 @@
+select name as Customers from Customers where id not in(select customerId from Orders)
+
+
+
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.customerId IS NULL;
